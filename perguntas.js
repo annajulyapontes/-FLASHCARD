727249c1940047ae6248a49c98083eb721bb7fc0
annajulyapontes <140862312+annajulyapontes@users.noreply.github.com>
2024-09-27 22:06:07 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Felicidades',
+    'O que precisa para ser feliz?',
+    ' Fazer o que gosta!!!'
+)
+
+criaCartao(
+    'Informática',
+    'Qual o melhor canal para aprender informática',
+    '@rasinformática'
+)
+
+criaCartao(
+    'Qual é a melhor turma do CERB',
+    'A que mais se dedica as aulas do Prof. Rafael?',
+    'Estou em dúvida'
+)
+
+criaCartao(
+    'Ganhando o Mundo',
+    'Quem participou do Programa Ganhado o mundo em 2024?',
+    'Eloisa Amorim dos Santos'
+)
